f47b028301a8174f0f83c5bfb1b4975c5203c32b
Christopher Lopes <christopher.lopes@gmail.com>
2017-04-14 20:49:23 -0700

    Add new feed for myget

--- FeedScan/config.js
+++ FeedScan/config.js
@@ -2,6 +2,7 @@ module.exports = {
     table: 'feeds',
     feeds: [
         'https://www.archlinux.org/feeds/news/',
-        'https://azure.microsoft.com/en-us/blog/topics/developer/feed/'
+        'https://azure.microsoft.com/en-us/blog/topics/developer/feed/',
+        'http://blog.myget.org/syndication.axd'
     ]
 };
