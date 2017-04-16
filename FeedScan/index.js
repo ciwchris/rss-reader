cc814d66862c1935f3fa54c1a1ca42bb676505c1
Christopher Lopes <christopher.lopes@gmail.com>
2017-04-16 08:50:30 -0700

    Move to feed-read-parser library
    The MyGet feed couldn't be parsed with the other library.

--- FeedScan/index.js
+++ FeedScan/index.js
@@ -1,4 +1,4 @@
-var feed = require("feed-read");
+var feedReader = require('feed-read-parser');
 var azure = require('azure-storage');
 var async = require('async');
 var config = require('./config');
@@ -13,53 +13,40 @@ module.exports = function (context, myTimer) {
         if(error){
             context.log('Error creating table: ' + JSON.stringify(error));
         } else {
-            async.parallel(
-            config.feeds.map(feed => function (callback) { retrieveFeedContent(callback, feed); }),
-            function(error, results) {
-                if (error) context.done(null, {status: 500, body: JSON.stringify(error)})
-                else context.done(null, {body: 'Scan completed'})
+            feedReader(config.feeds, function(error, articles) {
+                if (error) {
+                    context.done(null, {status: 500, body: JSON.stringify(error)})
+                } else {
+                    async.filter(articles, function(article, callback) {
+                        var query = new azure.TableQuery()
+                            .where('PartitionKey eq ? && link eq ?', encodeURIComponent(article.feed.source), article.link);
+                        tableSvc.queryEntities(config.table, query, null, function(error, result, response) {
+                            callback(null, result.entries.length === 0);
+                        });
+                    },
+                    function(error, results) {
+                        context.log('inserting: ' + results.length);
+                        async.parallel(
+                        results.map(article => function (callback) { loadFeedEntries(callback, article); }),
+                        function(error, results) {
+                            if (error) context.done(null, {status: 500, body: JSON.stringify(error)})
+                            else context.done(null, {body: 'Scan completed'})
+                        });
+                    });
+                }
             });
         }
     });
 
-    function retrieveFeedContent(rootCallback, feedUrl) {
-        feed(feedUrl, function(error, articles) {
-            if (error) {
-                context.log('Error loading feed: ' + feedUrl);
-                rootCallback(null, []);
-            } else {
-                addNewEntries(rootCallback, feedUrl, articles);
-            }
-        });
-    }
-
-    function addNewEntries(rootCallback, feedUrl, articles) {
-        async.filter(articles, function(article, callback) {
-            var query = new azure.TableQuery()
-                .where('PartitionKey eq ? && link eq ?', encodeURIComponent(feed), article.link);
-            tableSvc.queryEntities(config.table, query, null, function(error, result, response) {
-                callback(null, result.entries.length === 0);
-            });
-        },
-        function(error, results) {
-            context.log('inserting: ' + results.length);
-            async.parallel(
-            results.map(article => function (callback) { loadFeedEntries(callback, feedUrl, article); }),
-            function(error, results) {
-                rootCallback(null, null);
-            });
-        });
-    }
-
-    function loadFeedEntries(callback, feed, article) {
+    function loadFeedEntries(callback, article) {
         var entGen = azure.TableUtilities.entityGenerator;
         var task = {
-            PartitionKey: entGen.String(encodeURIComponent(feed)),
+            PartitionKey: entGen.String(encodeURIComponent(article.feed.source)),
             RowKey: entGen.String(999999999999999 - new Date().getTime() + '-' + encodeURIComponent(article.link)),
             title: entGen.String(article.title),
             link: entGen.String(article.link),
             published: entGen.DateTime(article.published),
-            feed: entGen.String(article.feed.name)
+            feed: entGen.String(article.feed.source)
             //content: entGen.String(article.content)
         };
 
