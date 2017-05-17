b305a52bcbc1c39bff3fc91b4deb068646bc7bed
Christopher Lopes <christopher.lopes@gmail.com>
2017-05-16 19:22:45 -0700

    Add James Reeves' blog

--- FeedScan/config.js
+++ FeedScan/config.js
@@ -4,6 +4,7 @@ module.exports = {
         'https://www.archlinux.org/feeds/news/',
         'http://blog.myget.org/syndication.axd',
         'https://azure.microsoft.com/en-us/blog/topics/developer/feed/',
+        'https://www.booleanknot.com/blog/feed.xml',
         'http://rss-reader-test-blog.blogspot.com/feeds/posts/default?alt=rss'
     ]
 };
