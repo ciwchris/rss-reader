063d7b5a08fa6b524a672bcf53a7b8c5d1389b8a
Christopher Lopes <christopher.lopes@gmail.com>
2017-07-04 14:59:33 -0700

    Add odetocode feed

--- FeedScan/config.js
+++ FeedScan/config.js
@@ -5,6 +5,7 @@ module.exports = {
         'http://blog.myget.org/syndication.axd',
         'https://azure.microsoft.com/en-us/blog/topics/developer/feed/',
         'https://www.booleanknot.com/blog/feed.xml',
+        'http://feeds.feedburner.com/OdeToCode',
         'http://rss-reader-test-blog.blogspot.com/feeds/posts/default?alt=rss'
     ]
 };
