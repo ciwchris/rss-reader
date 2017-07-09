270e4b7d867fbdc88036b26bb70e301ed0da8ac1
Christopher Lopes <christopher.lopes@gmail.com>
2017-07-08 17:53:50 -0700

    Add feed for windows command line tools

--- FeedScan/config.js
+++ FeedScan/config.js
@@ -6,6 +6,7 @@ module.exports = {
         'https://azure.microsoft.com/en-us/blog/topics/developer/feed/',
         'https://www.booleanknot.com/blog/feed.xml',
         'http://feeds.feedburner.com/OdeToCode',
+        'https://blogs.msdn.microsoft.com/commandline/feed/',
         'http://rss-reader-test-blog.blogspot.com/feeds/posts/default?alt=rss'
     ]
 };
