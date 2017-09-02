feb53e166bea60f41e12006e7c078191b6cef637
Christopher Lopes <christopher.lopes@gmail.com>
2017-09-02 14:29:25 -0700

    Update MyGet feed to new location

--- FeedScan/config.js
+++ FeedScan/config.js
@@ -2,11 +2,13 @@ module.exports = {
     table: 'feeds',
     feeds: [
         'https://www.archlinux.org/feeds/news/',
-        'http://blog.myget.org/syndication.axd',
+        'https://blog.myget.org/feed',
         'https://azure.microsoft.com/en-us/blog/topics/developer/feed/',
         'https://www.booleanknot.com/blog/feed.xml',
         'http://feeds.feedburner.com/OdeToCode',
         'https://blogs.msdn.microsoft.com/commandline/feed/',
-        'http://rss-reader-test-blog.blogspot.com/feeds/posts/default?alt=rss'
+        'http://rss-reader-test-blog.blogspot.com/feeds/posts/default?alt=rss',
+        'https://azure.microsoft.com/en-us/roadmap/azure-functions-visual-studio-2017-azure-functions-tooling/feed/',
+        'https://blog.angularindepth.com/feed'
     ]
 };
